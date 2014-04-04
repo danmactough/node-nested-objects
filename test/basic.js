d950eeceb23aed8b4eb3b05b9f6f3fc557967576
Dan MacTough <danmactough@gmail.com>
2014-04-04 08:06:16 -0700

    Initial commit

---
+++ test/basic.js
@@ -0,0 +1,6 @@
+describe('basic', function () {
+  it('can get nested a nested property');
+  it('can set nested a nested property');
+  it('can delete nested a nested property');
+  it('can flatten a nested object');
+});
