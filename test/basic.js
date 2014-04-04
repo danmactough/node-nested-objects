493081073f807e93e0a504b7ef355e881a77ed79
Dan MacTough <danmactough@gmail.com>
2014-04-04 12:12:11 -0400

    Add tests

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,47 @@
 describe('basic', function () {
-  it('can get nested a nested property');
-  it('can set nested a nested property');
-  it('can delete nested a nested property');
-  it('can flatten a nested object');
+  var assert = require('assert');
+  var nested = require('../');
+  var source = {
+    a: 1,
+    b: {
+      A: 'i',
+      B: 'ii'
+    },
+    c: {
+      A: {
+        i: [ 'apples', 'bananas', 'coconuts' ],
+        ii: [ 'ankles', 'bones', 'curly hair' ]
+      },
+      B: {
+        i: [ 'waffles', 'pancakes' ],
+        ii: [ 'movies', 'buttons', 'heart attacks' ]
+      }
+    }
+  };
+
+  it('can get nested a nested property', function () {
+    assert.strictEqual(nested.get(source, 'a'), source.a);
+    assert.strictEqual(nested.get(source, 'b.A'), source.b.A);
+    assert.strictEqual(nested.get(source, 'c.A.i'), source.c.A.i);
+  });
+  it('can set nested a nested property', function () {
+    var arr = [ 'apples', 'bananas', 'coconuts', 'donuts' ];
+    nested.set(source, 'c.A.i', nested.get(source, 'c.A.i').concat(arr[3]));
+    assert.strictEqual(nested.get(source, 'c.A.i').length, 4);
+    for (var i in arr) {
+      assert.strictEqual(nested.get(source, 'c.A.i')[i], arr[i]);
+    }
+  });
+  it('can delete nested a nested property', function () {
+    nested.delete(source, 'b.B');
+    assert.strictEqual(source.b.A, 'i');
+    assert.strictEqual(source.b.B, undefined);
+  });
+  it('can flatten a nested object', function () {
+    var deep = { a: { b: [ 1, 2, 3 ] } };
+    var flat = { 'a.b.0': 1,
+                 'a.b.1': 2,
+                 'a.b.2': 3 };
+    assert.deepEqual(nested.flatten(deep), flat);
+  });
 });
